48ca8a5d9f1570cab7421b9eb0ba3616e411492e
Mauro Lopes <maurolopes@gmail.com>
2014-03-31 05:21:10 -0300

    Create tests for all requests

--- src/app.js
+++ src/app.js
@@ -47,3 +47,5 @@ app.get('/recommendations', function (req, res) {
 var server = app.listen(3000, function () {
   console.log('Listening on port %d', server.address().port);
 });
+
+exports.app = app;
---
+++ test/test.js
@@ -0,0 +1,75 @@
+"use strict";
+
+var supertest = require('supertest');
+var app = require('../src/app.js').app;
+var assert = require('assert');
+
+describe('Recommendation system:', function () {
+
+  describe('POST /follow', function () {
+    it('should return an error code if some parameters are missing', function (done) {
+      supertest(app)
+        .post('/follow')
+        .end(function (err, res) {
+          assert.equal(res.statusCode, 400); //Bad request
+          done();
+        });
+    });
+
+    it('should correctly add a follower', function (done) {
+      supertest(app)
+        .post('/follow')
+        .send({from: "user123", to: "user456"})
+        .end(function (err, res) {
+          assert.equal(res.statusCode, 200); //OK
+          done();
+        });
+    });
+  })
+
+
+  describe('POST /listen', function () {
+    it('should return an error code if some parameters are missing', function (done) {
+      supertest(app)
+        .post('/listen')
+        .end(function (err, res) {
+          assert.equal(res.statusCode, 400); //Bad request
+          done();
+        });
+    });
+
+    it('should correctly add a listened song', function (done) {
+      supertest(app)
+        .post('/listen')
+        .send({user: "user123", music: "music123"})
+        .end(function (err, res) {
+          assert.equal(res.statusCode, 200); //OK
+          done();
+        });
+    });
+  })
+
+
+  describe('GET /recommendations', function () {
+    it('should return an error code if some parameters are missing', function (done) {
+      supertest(app)
+        .get('/recommendations')
+        .end(function (err, res) {
+          assert.equal(res.statusCode, 400); //Bad request
+          done();
+        });
+    });
+
+    it('should return an object with a list of 5 strings', function (done) {
+      supertest(app)
+        .get('/recommendations?user=aaaa')
+        .end(function (err, res) {
+          assert.equal(res.statusCode, 200); //OK
+          assert.notEqual(res.body, undefined);
+          assert.notEqual(res.body.list, undefined);
+          assert.equal(res.body.list.length, 5);
+          done();
+        });
+    });
+  })
+});
